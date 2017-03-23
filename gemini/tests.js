c715cd1f6b5b4d2ec4b886423a7354b2f3896bf1
Limon Monte <limon.monte@gmail.com>
2017-03-23 11:45:54 +0200

    wip

--- gemini/tests.js
+++ gemini/tests.js
@@ -3,7 +3,7 @@ gemini.suite('basic', (suite) => {
     .setCaptureElements('.swal2-modal')
     .capture('html-content', (actions, find) => {
       actions.click(find('.html button'))
-      actions.wait(300)
+      actions.wait(500)
     })
 })
 
@@ -14,13 +14,13 @@ gemini.suite('ajax-request', (suite) => {
       actions.click(find('.ajax-request button'))
       actions.sendKeys(find('.swal2-input'), 'taken@example.com')
       actions.click(find('.swal2-confirm'))
-      actions.wait(3000)
+      actions.wait(5000)
     })
     .capture('success', (actions, find) => {
       actions.sendKeys(find('.swal2-input'), gemini.BACK_SPACE)
       actions.wait(100)
       actions.click(find('.swal2-confirm'))
-      actions.wait(3000)
+      actions.wait(5000)
     })
 })
 
@@ -30,20 +30,20 @@ gemini.suite('chaining-modals', (suite) => {
     .capture('step 1', (actions, find) => {
       actions.click(find('.chaining-modals button'))
       actions.sendKeys(find('.swal2-input'), 'uno')
-      actions.wait(300)
+      actions.wait(500)
     })
     .capture('step 2', (actions, find) => {
       actions.click(find('.swal2-confirm'))
       actions.sendKeys(find('.swal2-input'), 'dos')
-      actions.wait(300)
+      actions.wait(500)
     })
     .capture('step 3', (actions, find) => {
       actions.click(find('.swal2-confirm'))
       actions.sendKeys(find('.swal2-input'), 'tres')
-      actions.wait(300)
+      actions.wait(500)
     })
     .capture('confirm', (actions, find) => {
       actions.click(find('.swal2-confirm'))
-      actions.wait(300)
+      actions.wait(500)
     })
 })
