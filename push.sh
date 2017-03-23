55f929517a38e52cce145147ee43da218b254961
Limon Monte <limon.monte@gmail.com>
2017-03-23 13:00:39 +0200

    push.sh

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git status
+git add .
+git status
+echo "Travis build: ${TRAVIS_BUILD_NUMBER}"
+git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+echo $branch_name
+branch_name=${branch_name##refs/heads/}
+echo $branch_name
+git push --set-upstream origin $branch_name
